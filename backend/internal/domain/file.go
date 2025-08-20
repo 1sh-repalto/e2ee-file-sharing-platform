11dfdde5766d51152f477657f04d32d88d6499bf
Vansh Negi <vanshrepalto@gmail.com>
2025-08-20 14:56:10 +0000

    minor fix in file and share models

--- backend/internal/domain/file.go
+++ backend/internal/domain/file.go
@@ -7,10 +7,12 @@ import (
 )
 
 type File struct {
-	ID        uuid.UUID `db:"id"`
-	OwnerID   uuid.UUID `db:"owner_id"`
-	Name      string    `db:"name"`
-	Size      int64     `db:"size"`
-	IV        []byte    `db:"iv"`
-	CreatedAt time.Time `db:"created_at"`
+	ID        		uuid.UUID	`db:"id"`
+	OwnerID   		uuid.UUID	`db:"owner_id"`
+	Filename 		string      `db:"name"`
+	MimeType  		string	    `db:"mime_type"`
+	Size      		int64       `db:"size"`
+	IV        		[]byte      `db:"iv"`
+	Encrypted_key	[]byte	    `db:"encrypted_key"`
+	CreatedAt 		time.Time   `db:"created_at"`
 }
