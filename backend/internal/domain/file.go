41bf1461de67f8db2a7754105efc3576920f8633
Vansh Negi <vanshrepalto@gmail.com>
2025-08-20 14:59:27 +0000

    minor fix in file model

--- backend/internal/domain/file.go
+++ backend/internal/domain/file.go
@@ -9,10 +9,10 @@ import (
 type File struct {
 	ID        		uuid.UUID	`db:"id"`
 	OwnerID   		uuid.UUID	`db:"owner_id"`
-	Filename 		string      `db:"name"`
+	Filename 		string      `db:"filename"`
 	MimeType  		string	    `db:"mime_type"`
 	Size      		int64       `db:"size"`
 	IV        		[]byte      `db:"iv"`
-	Encrypted_key	[]byte	    `db:"encrypted_key"`
+	EncryptedKey	[]byte	    `db:"encrypted_key"`
 	CreatedAt 		time.Time   `db:"created_at"`
 }
